4012b195b8ce64f64c433d5f79dde3aad737db22
tehjinxi <henryteh.1013@gmail.com>
2020-04-18 00:19:02 +0800

    finish get latest pi data

--- app/src/main/java/com/example/smarthome/GetDataActivity.kt
+++ app/src/main/java/com/example/smarthome/GetDataActivity.kt
@@ -1,15 +1,23 @@
 package com.example.smarthome
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_get_data.*
+import java.time.LocalDateTime
+import java.util.Calendar
+import kotlin.time.ExperimentalTime
+import java.time.format.DateTimeFormatter
 
 class GetDataActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    @ExperimentalTime
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_data)
@@ -18,22 +26,44 @@ class GetDataActivity : AppCompatActivity() {
         actionbar!!.title = "Latest Data"
         actionbar.setDisplayHomeAsUpEnabled(true)
 
-        getData()
+        val current = LocalDateTime.now()
+
+        val date1 = DateTimeFormatter.ofPattern("yyyyMMdd")
+        val dated = current.format(date1)
+        val hour1 =  DateTimeFormatter.ofPattern("HH")
+        val houred = current.format(hour1)
+        val time1 =  DateTimeFormatter.ofPattern("mmss")
+        val timed = current.format(time1)
+
+        val path1 = "PI_01_"+dated
+        //path 2 is hour
+        val path3 = timed.substring(0,3) + "0"
+        datetimeView.text = path1+" "+houred+" "+path3
+
+        getData(path1,houred,path3)
     }
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
     }
 
-    private fun getData(){
-        //var PiData: PiData? = null  // declare user object outside onCreate Method
-
-        var ref = FirebaseDatabase.getInstance().getReference("PI_01_20200417").child("00").child("0000")
+    private fun getData(path1:String,path2:String,path3:String){
+        var ref = FirebaseDatabase.getInstance().getReference(path1).child(path2).child(path3)
 
         val menuListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val PiData = dataSnapshot.getValue()
-                textView.text = PiData.toString()
+                val piData = dataSnapshot.getValue(PiData::class.java)
+                if(piData != null){
+                    humidView.text = piData.humid
+                    lightView.text = piData.light
+                    soundView.text = piData.sound
+                    tempeView.text = piData.tempe
+                    ultraView.text = piData.ultra
+                }
+                else{
+                    humidView.text = "null"
+                }
+
             }
             override fun onCancelled(databaseError: DatabaseError) {
                 // handle error
--- app/src/main/java/com/example/smarthome/PiData.kt
+++ app/src/main/java/com/example/smarthome/PiData.kt
@@ -1,9 +1,14 @@
 package com.example.smarthome
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 class PiData (
     val humid: String,
-    val light:String,
-    val sound:String,
+    val light: String,
+    val sound: String,
     val tempe: String,
     val ultra: String
-)
+): Parcelable {
+    constructor() : this("", "", "", "", "")
+}
